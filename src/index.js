72ea7b1711a309ce9ee4b21508431f03a4179b9d
Alex Zdch <pachtouka@gmail.com>
2020-10-11 22:55:32 +0300

    feat: add function sort

--- src/index.js
+++ src/index.js
@@ -1,6 +1,27 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort (array = []) {
+  var resultArray = [];
+     
+  function funMin(a,b){
+     return a-b;
+  }
+
+  function funMax(a,b){
+     return b-a;
+  }
+
+
+ for ( var i = 0; i < array.length; i ++) {
+    if  ( !( i % 2) ){
+       array[i].sort(funMin);
+    } else {
+   
+       array[i].sort(funMax);
+   }
+    resultArray = resultArray.concat (array[i]);
+  }
+
+return resultArray;
 }
